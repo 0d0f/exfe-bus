9c9cd67828d05a4962915885d44882854755c8f9
Googol Lee <googollee@gmail.com>
2012-02-28 17:40:05 +0800

    update sendtweet service using gobus

--- twitter/sendtweet.go
+++ twitter/sendtweet.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"oauth"
-	"gosque"
+	"gobus"
 	"net/url"
 	"fmt"
+	"io/ioutil"
 )
 
-type Tweet struct {
+type TweetService struct {
 	ClientToken string
 	ClientSecret string
 	AccessToken string
@@ -15,40 +16,45 @@ type Tweet struct {
 	Tweet string
 }
 
-func (t *Tweet) GoString() string {
+func (t *TweetService) GoString() string {
 	return fmt.Sprintf("{Client:(%s %s) Access:(%s %s) Tweet:%s}", t.ClientToken, t.ClientSecret, t.AccessToken, t.AccessSecret, t.Tweet)
 }
 
-const (
-	queue = "resque:twitter:tweet"
-	maxJobs = 10
-	timeOut = 5e9 // 5 seconds
-)
-
-func generateEmptyTweet() interface{} {
-	return &Tweet{}
-}
-
-func sendTweet(tweet *Tweet) error {
+func (t *TweetService) Do(jobs []interface{}) []interface{} {
+	tweet := jobs[0].(*TweetService)
 	client := oauth.CreateClient(tweet.ClientToken, tweet.ClientSecret, tweet.AccessToken, tweet.AccessSecret, "https://api.twitter.com/1/")
 	params := make(url.Values)
 	params.Add("status", tweet.Tweet)
-	_, err := client.Do("POST", "/statuses/update.json", params)
-	return err
+
+	retReader, err := client.Do("POST", "/statuses/update.json", params)
+	if err != nil {
+		return []interface{}{map[string]string{"result": "", "error": err.Error()}, }
+	}
+
+	retBytes, err := ioutil.ReadAll(retReader)
+	if err != nil {
+		return []interface{}{map[string]string{"result": "", "error": err.Error()}, }
+	}
+
+	return []interface{}{map[string]string{"result": string(retBytes), "error": ""}, }
 }
 
+func (t *TweetService) MaxJobsCount() int {
+	return 1
+}
+
+func (t *TweetService) JobGenerator() interface{} {
+	return &TweetService{}
+}
+
+const (
+	queue = "gobus:queue:twitter:tweet"
+	timeOut = 5e9 // 5 seconds
+)
+
 func main() {
-	gosque := gosque.CreateQueue("", 0, "", queue)
-	defer func() { gosque.Close() }()
+	service := gobus.CreateService("", 0, "", queue, &TweetService{})
+	defer service.Close()
 
-	jobRecv := gosque.IncomingJob(generateEmptyTweet, timeOut)
-	for {
-		job := (<-jobRecv).(*Tweet)
-		fmt.Println("Process job: ", job.Tweet)
-
-		err := sendTweet(job)
-		if err != nil {
-			fmt.Printf("Send tweet (%s) failed: %s\n", job, err)
-		}
-	}
+	service.Run(timeOut)
 }
--- twitter/sendtweet_test.rb
+++ twitter/sendtweet_test.rb
@@ -1,16 +1,20 @@
 #!/usr/bin/env ruby
 # coding: utf-8
 
-data = {
-  "ClientToken" => "VC3OxLBNSGPLOZ2zkgisA",
-  "ClientSecret" => "Lg6b5eHdPLFPsy4pI2aXPn6qEX6oxTwPyS0rr2g4A",
-  "AccessToken" => "491159882-urND5ZaHmUPWNgvpr5coIifkApmKsmjGtX69Bn51",
-  "AccessSecret" => "5kwJdfqd6xL93BvPisYaRVzk5VlOEMhQwAk2aPMxy6s",
-  "Tweet" => "就是测个试",
+require 'json'
+require 'redis'
+
+queue = "gobus:queue:twitter:tweet"
+tweet = {
+  :ResponseKey => "#{queue}:1",
+  :Data => {
+    :ClientToken => "VC3OxLBNSGPLOZ2zkgisA",
+    :ClientSecret => "Lg6b5eHdPLFPsy4pI2aXPn6qEX6oxTwPyS0rr2g4A",
+    :AccessToken => "491159882-urND5ZaHmUPWNgvpr5coIifkApmKsmjGtX69Bn51",
+    :AccessSecret => "5kwJdfqd6xL93BvPisYaRVzk5VlOEMhQwAk2aPMxy6s",
+    :Tweet => "就是测个试",
+  }
 }
 
-require 'redis'
-require 'json'
-
 redis = Redis.new
-redis.rpush 'resque:twitter:tweet', data.to_json
+redis.rpush queue, tweet.to_json
