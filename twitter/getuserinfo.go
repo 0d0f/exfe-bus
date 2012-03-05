4029068478c559466ad4bc81acfa66b86c3f4fa7
Googol Lee <googollee@gmail.com>
2012-03-05 17:30:48 +0800

    add getuserinfo service

---
+++ twitter/getuserinfo.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"oauth"
+	"gobus"
+	"net/url"
+	"fmt"
+	"io/ioutil"
+)
+
+type UserInfo struct {
+	ClientToken string
+	ClientSecret string
+	AccessToken string
+	AccessSecret string
+
+	UserId string
+	ScreenName string
+}
+
+func (i *UserInfo) GoString() string {
+	return fmt.Sprintf("{Client:(%s %s) Access:(%s %s) User %s(%s)}",
+		i.ClientToken, i.ClientSecret, i.AccessToken, i.AccessSecret, i.ScreenName, i.UserId)
+}
+
+func (i *UserInfo) Do(messages []interface{}) []interface{} {
+	message := messages[0].(*UserInfo)
+	client := oauth.CreateClient(message.ClientToken, message.ClientSecret, message.AccessToken, message.AccessSecret, "https://api.twitter.com/1/")
+	params := make(url.Values)
+	if message.ScreenName != "" {
+		params.Add("screen_name", message.ScreenName)
+	} else {
+		params.Add("user_id", message.UserId)
+	}
+	retReader, err := client.Do("GET", "/users/show.json", params)
+	if err != nil {
+		return []interface{}{map[string]string{"error": err.Error()}}
+	}
+
+	retBytes, err := ioutil.ReadAll(retReader)
+	if err != nil {
+		return []interface{}{map[string]string{"error": err.Error()}}
+	}
+
+	// TODO:
+	// twitter info update
+	return []interface{}{map[string]string{"result": string(retBytes)}}
+}
+
+func (i *UserInfo) MaxJobsCount() int {
+	return 1
+}
+
+func (i *UserInfo) JobGenerator() interface{} {
+	return &UserInfo{}
+}
+
+const (
+	queue = "gobus:queue:twitter:userinfo"
+	timeOut = 5e9 // 5 seconds
+)
+
+func main() {
+	service := gobus.CreateService("", 0, "", queue, &UserInfo{})
+	defer func() {
+		service.Close()
+		service.Clear()
+	}()
+
+	service.Run(timeOut)
+}
---
+++ twitter/getuserinfo_test.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+
+require 'json'
+require 'redis'
+
+queue = "gobus:queue:twitter:userinfo"
+job1 = {
+  :ResponseKey => "#{queue}:1",
+  :Data => {
+    :ClientToken => "VC3OxLBNSGPLOZ2zkgisA",
+    :ClientSecret => "Lg6b5eHdPLFPsy4pI2aXPn6qEX6oxTwPyS0rr2g4A",
+    :AccessToken => "491159882-urND5ZaHmUPWNgvpr5coIifkApmKsmjGtX69Bn51",
+    :AccessSecret => "5kwJdfqd6xL93BvPisYaRVzk5VlOEMhQwAk2aPMxy6s",
+    :ScreenName => "lzh429",
+  }
+}
+
+job2 = {
+  :ResponseKey => "#{queue}:2",
+  :Data => {
+    :ClientToken => "VC3OxLBNSGPLOZ2zkgisA",
+    :ClientSecret => "Lg6b5eHdPLFPsy4pI2aXPn6qEX6oxTwPyS0rr2g4A",
+    :AccessToken => "491159882-urND5ZaHmUPWNgvpr5coIifkApmKsmjGtX69Bn51",
+    :AccessSecret => "5kwJdfqd6xL93BvPisYaRVzk5VlOEMhQwAk2aPMxy6s",
+    :UserId => "491159882",
+  }
+}
+
+redis = Redis.new
+redis.rpush queue, job1.to_json
+redis.rpush queue, job2.to_json
