a5441855f775f3bab16a72bd418e40450d7bf716
Googol Lee <googollee@gmail.com>
2012-03-14 11:39:30 +0800

    update getuserinfo service

--- twitter/getuserinfo.go
+++ twitter/getuserinfo.go
@@ -16,49 +16,36 @@ type UserInfoService struct {
 	info *twitter.UserInfo
 }
 
-func (i *UserInfoService) Do(messages []interface{}) []interface{} {
-	data, ok := messages[0].(*UserInfoService)
-	if !ok {
-		log.Printf("Can't convert input into UserInfo: %s", messages)
-		return nil
-	}
+func (i *UserInfoService) Do(arg twitter.UserInfo, reply *string) error {
+	log.Printf("Try to get %s(%s) userinfo...", arg.ScreenName, arg.UserId)
 
-	log.Printf("Try to get %s(%s) userinfo...", data.info.ScreenName, data.info.UserId)
-
-	client := oauth.CreateClient(data.info.ClientToken, data.info.ClientSecret, data.info.AccessToken, data.info.AccessSecret, "https://api.twitter.com/1/")
+	client := oauth.CreateClient(arg.ClientToken, arg.ClientSecret, arg.AccessToken, arg.AccessSecret, "https://api.twitter.com/1/")
 	params := make(url.Values)
-	if data.info.ScreenName != "" {
-		params.Add("screen_name", data.info.ScreenName)
+	if arg.ScreenName != "" {
+		params.Add("screen_name", arg.ScreenName)
 	} else {
-		params.Add("user_id", data.info.UserId)
+		params.Add("user_id", arg.UserId)
 	}
 	retReader, err := client.Do("GET", "/users/show.json", params)
 	if err != nil {
 		log.Printf("Twitter access error: %s", err)
-		return []interface{}{map[string]string{"error": err.Error()}}
+		return err
 	}
 
 	retBytes, err := ioutil.ReadAll(retReader)
 	if err != nil {
 		log.Printf("Can't load twitter response: %s", err)
-		return []interface{}{map[string]string{"error": err.Error()}}
+		return err
 	}
 
 	// TODO:
 	// twitter info update
-	return []interface{}{map[string]string{"result": string(retBytes)}}
-}
-
-func (i *UserInfoService) MaxJobsCount() int {
-	return 1
-}
-
-func (i *UserInfoService) JobGenerator() interface{} {
-	return &UserInfoService{}
+	*reply = string(retBytes)
+	return nil
 }
 
 const (
-	queue = "gobus:queue:twitter:userinfo"
+	queue = "twitter:userinfo"
 )
 
 func main() {
@@ -76,13 +63,12 @@ func main() {
 		config.Int("redis.db"),
 		config.String("redis.password"),
 		queue,
-		&UserInfoService{},
-		config.Int("service.limit"))
+		&UserInfoService{})
 	defer func() {
 		log.Printf("Service stop, queue: %s", queue)
 		service.Close()
 		service.Clear()
 	}()
 
-	service.Run(time.Duration(config.Int("service.time_out")))
+	service.Serve(time.Duration(config.Int("service.time_out")))
 }
--- twitter/getuserinfo_test.rb
+++ twitter/getuserinfo_test.rb
@@ -7,26 +7,26 @@ require 'redis'
 queue = "gobus:queue:twitter:userinfo"
 job1 = {
   :Id => "#{queue}:1",
-  :Data => {
+  :Arg => {
     :ClientToken => "VC3OxLBNSGPLOZ2zkgisA",
     :ClientSecret => "Lg6b5eHdPLFPsy4pI2aXPn6qEX6oxTwPyS0rr2g4A",
     :AccessToken => "491159882-urND5ZaHmUPWNgvpr5coIifkApmKsmjGtX69Bn51",
     :AccessSecret => "5kwJdfqd6xL93BvPisYaRVzk5VlOEMhQwAk2aPMxy6s",
     :ScreenName => "lzh429",
   },
-  :NeedReturn => true,
+  :NeedReply => true,
 }
 
 job2 = {
   :Id => "#{queue}:2",
-  :Data => {
+  :Arg => {
     :ClientToken => "VC3OxLBNSGPLOZ2zkgisA",
     :ClientSecret => "Lg6b5eHdPLFPsy4pI2aXPn6qEX6oxTwPyS0rr2g4A",
     :AccessToken => "491159882-urND5ZaHmUPWNgvpr5coIifkApmKsmjGtX69Bn51",
     :AccessSecret => "5kwJdfqd6xL93BvPisYaRVzk5VlOEMhQwAk2aPMxy6s",
     :UserId => "491159882",
   },
-  :NeedReturn => true,
+  :NeedReply => true,
 }
 
 redis = Redis.new
