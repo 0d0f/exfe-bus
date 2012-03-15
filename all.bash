bffa760e81d6c6ff5eb23c99aa3e4a93431f98ec
Googol Lee <googollee@gmail.com>
2012-03-15 18:46:00 +0800

    update build command

--- all.bash
+++ all.bash
@@ -1,11 +1,7 @@
 go get github.com/simonz05/godis
 go get github.com/googollee/go-gypsy
 go get github.com/garyburd/go-oauth
-GOPATH=$GOPATH:$(pwd) go install config
-GOPATH=$GOPATH:$(pwd) go install gobus
-GOPATH=$GOPATH:$(pwd) go install gosque
-GOPATH=$GOPATH:$(pwd) go install oauth
-GOPATH=$GOPATH:$(pwd) go install twitter/job
-GOPATH=$GOPATH:$(pwd) go install twitter/service
-GOPATH=$GOPATH:$(pwd) go install twitter
-GOPATH=$GOPATH:$(pwd) go install mail
+cat build.list | while read l;
+do
+  GOPATH=$GOPATH:$(pwd) go install $l
+done
