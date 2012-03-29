3c35b7d9e8d095b02289b87c460b4081e3c66e50
Googol Lee <googollee@gmail.com>
2012-03-29 15:48:37 +0800

    update to fit Go 1

--- all.bash
+++ all.bash
@@ -1,5 +1,12 @@
+GODIR=${GOPATH%%:*}
 go get github.com/simonz05/godis
+cd $GODIR/src/github.com/simonz05/godis
+git checkout stable
+cd -
 go get github.com/garyburd/go-oauth/oauth
+cd $GODIR/src/github.com/garyburd/go-oauth/oauth
+git checkout master
+cd -
 cat build.list | while read l;
 do
   GOPATH=$GOPATH:$(pwd) go install $l
