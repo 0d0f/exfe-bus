07dc068434dfc619f4997d5d30915fda5f06cb34
Googol Lee <googollee@gmail.com>
2012-05-14 12:33:14 +0800

    fix data file name

--- test_data/cross_test.rb
+++ test_data/cross_test.rb
@@ -5,7 +5,7 @@ require 'redis'
 redis = Redis.new
 i = 0
 
-open("./test_data/twitter_test.data").each do |l|
+open("./test_data/cross_test.data").each do |l|
   next if l.length == 0
   redis.rpush "gobus:queue:cross", l
   i += 1
