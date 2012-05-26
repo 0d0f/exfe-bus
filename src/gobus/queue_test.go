51f3d1ea704ad1d029cf0afa832ed46e68d0925a
Googol Lee <googollee@gmail.com>
2012-05-26 15:13:39 +0800

    add head delay queue

--- src/gobus/queue_test.go
+++ src/gobus/queue_test.go
@@ -75,3 +75,31 @@ func TestTailDelayQueue(t *testing.T) {
 		t.Fatalf("queue next wakeup should %d seconds, but got: %d", interval, next)
 	}
 }
+
+func TestHeadDelayQueue(t *testing.T) {
+	interval := 5
+
+	godis := godis.New("", 0, "")
+	var queueType []int
+	q, _ := NewHeadDelayQueue("hdt", interval, queueType, godis)
+
+	next, _ := q.NextWakeup()
+	if next != (time.Duration(interval) * time.Second) {
+		t.Fatalf("queue next wakeup should %d seconds, but got: %d", interval, next)
+	}
+
+	for i := 0; i < 10; i++ {
+		q.Push("googollee", i)
+	}
+	next, _ = q.NextWakeup()
+	time.Sleep(next)
+
+	q.Push("googollee", 10)
+	ret, err := q.Pop()
+	if err != nil {
+		t.Fatalf("Pop error: %s", err)
+	}
+	if ret == nil {
+		t.Fatalf("Pop should get anything, but got nothing")
+	}
+}
