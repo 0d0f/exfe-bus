ea79ee9de5cdf3fa7a3dd610a4931ba6504b35cb
Googol Lee <googollee@gmail.com>
2012-06-15 19:25:14 +0800

    update template

--- src/exfe/model/post.go
+++ src/exfe/model/post.go
@@ -1,5 +1,10 @@
 package exfe_model
 
+import (
+	"time"
+	"fmt"
+)
+
 type Post struct {
 	Id uint64
 	By_identity Identity
@@ -7,5 +12,19 @@ type Post struct {
 	Postable_id uint64
 	Postable_type string
 	Via string
-	Relative map[string]string
+	Create_at string
+	/*Relative map[string]string*/
+}
+
+func (p *Post) CreateAt(timezone string) (string, error) {
+	t, err := time.Parse("2006-01-02 15:04:05 -0700", p.Create_at)
+	if err != nil {
+		return "", err
+	}
+	loc, err := LoadLocation(timezone)
+	if err != nil {
+		return "", fmt.Errorf("Parse target zone error: %s", err)
+	}
+	t = t.In(loc)
+	return t.Format("03:04PM Mon, Jan 2"), nil
 }
