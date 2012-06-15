c6626192ef3591e28ec6078c3d1517702b9902df
Googol Lee <googollee@gmail.com>
2012-06-15 16:40:29 +0800

    script update

--- all.bash
+++ all.bash
@@ -1,8 +1,8 @@
-go get github.com/googollee/godis
-go get github.com/garyburd/go-oauth/oauth
-go get github.com/virushuo/Go-Apns
-go get github.com/googollee/go_c2dm
+go get $1 github.com/googollee/godis
+go get $1 github.com/garyburd/go-oauth/oauth
+go get $1 github.com/virushuo/Go-Apns
+go get $1 github.com/googollee/go_c2dm
 cat build.list | while read l;
 do
-  GOPATH=$GOPATH:$(pwd) go install $l
+  GOPATH=$GOPATH:$(pwd) go install $1 $l
 done
--- deploy.bash
+++ deploy.bash
@@ -3,9 +3,9 @@ rm -Rf ./deploy
 mkdir -p ./deploy/usr/local/bin
 mkdir -p ./deploy/usr/local/etc
 
-cp -Rf ./bin/twitter ./deploy/usr/local/bin/exfebus_twitter
+ls ./bin | while read l; do cp -Rf ./bin/$l ./deploy/usr/local/bin/exfebus_$l; done
 cp -Rf ./template ./deploy/usr/local/etc/
 
 cd ./deploy
 rsync --progress -av * root@exfe.com:/
-ssh root@exfe.com 'kill `cat /var/run/resque/worker_exfe_twitter.pid`'
+ssh root@exfe.com 'ls /var/run/resque/worker_exfe_*.pid | while read l; do kill `cat $l`; done'
