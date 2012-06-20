683ca77e12a3c3c0f89f22c8ef42d9b4d025997d
Googol Lee <googollee@gmail.com>
2012-06-20 17:19:45 +0800

    add deploy dev

---
+++ deploy_dev.bash
@@ -0,0 +1,6 @@
+sudo /usr/local/etc/rc.d/monit stop
+ls /var/run/resque/worker_exfe_*.pid | while read l; do sudo kill `cat $l`; done
+ls bin/ | while read l; do sudo cp bin/$l /usr/local/bin/exfebus_$l; done
+sudo rm -r /usr/local/etc/exfebus/template
+sudo cp -r template /usr/local/etc/exfebus/
+sudo /usr/local/etc/rc.d/monit start
