484187c87f0239bff02002f2a11f7b255bba0063
Googol Lee <googollee@gmail.com>
2012-07-05 16:01:03 +0800

    update all.bash

--- all.bash
+++ all.bash
@@ -4,6 +4,7 @@ go get $1 github.com/virushuo/Go-Apns
 go get $1 github.com/googollee/go_c2dm
 go get $1 github.com/googollee/goimap
 go get $1 github.com/googollee/go-encoding-ex
+go get $1 github.com/sloonz/go-iconv
 cat build.list | while read l;
 do
   GOPATH=$GOPATH:$(pwd) go install $1 $l
