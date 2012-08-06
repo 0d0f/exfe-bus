883bbade9a5faed8ee66b437404130c8415a3ca5
Googol Lee <googollee@gmail.com>
2012-08-06 17:01:08 +0800

    remove twitter post

--- src/exfe/service/cross_post.go
+++ src/exfe/service/cross_post.go
@@ -5,12 +5,8 @@ import (
 	"c2dm/service"
 	"fmt"
 	"gobus"
-	"io/ioutil"
 	"log"
-	"net/http"
-	"net/url"
 	"os"
-	"twitter/service"
 )
 
 type CrossPost struct {
@@ -23,7 +19,6 @@ type CrossPost struct {
 
 func NewCrossPost(config *Config) *CrossPost {
 	return &CrossPost{
-		twitter: gobus.CreateClient(config.Redis.Netaddr, config.Redis.Db, config.Redis.Password, "twitter"),
 		ios:     gobus.CreateClient(config.Redis.Netaddr, config.Redis.Db, config.Redis.Password, "iOSAPN"),
 		android: gobus.CreateClient(config.Redis.Netaddr, config.Redis.Db, config.Redis.Password, "Android"),
 		log:     log.New(os.Stderr, "exfe.cross.post", log.LstdFlags),
@@ -34,73 +29,10 @@ func NewCrossPost(config *Config) *CrossPost {
 func (s *CrossPost) SendPost(arg *OneIdentityUpdateArg) {
 	by := arg.By_identity.Name
 	switch arg.To_identity.Provider {
-	case "twitter":
-		if arg.By_identity.Provider == "twitter" {
-			by = fmt.Sprintf("@%s", arg.By_identity.External_username)
-		}
-		s.SendTwitter(arg, fmt.Sprintf("%s %s", by, arg.Post.Content))
 	case "iOSAPN":
 		s.SendApn(arg, fmt.Sprintf("%s %s", by, arg.Post.Content))
 	case "Android":
 		s.SendAndroid(arg, fmt.Sprintf("%s %s", by, arg.Post.Content))
-	}
-}
-
-func (s *CrossPost) SendTwitter(arg *OneIdentityUpdateArg, msg string) {
-	params := make(url.Values)
-	params.Add("data", fmt.Sprintf("%s", arg.Cross.Id))
-	resp, err := http.PostForm(fmt.Sprintf("%s/%d", s.config.Iom_url, arg.To_identity.Id), params)
-	if err != nil {
-		s.log.Printf("access iom server error: %s", err)
-		return
-	}
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		s.log.Printf("read response from iom server error: %s", err)
-		return
-	}
-	if resp.StatusCode != 200 {
-		s.log.Printf("iom server return error(%s): %s", resp.Status, string(body))
-		return
-	}
-	urls := []string{fmt.Sprintf("#%s", string(body))}
-
-	isFriend := false
-	f := &twitter_service.FriendshipsExistsArg{
-		ClientToken:  s.config.Twitter.Client_token,
-		ClientSecret: s.config.Twitter.Client_secret,
-		AccessToken:  s.config.Twitter.Access_token,
-		AccessSecret: s.config.Twitter.Access_secret,
-		UserA:        arg.To_identity.External_username,
-		UserB:        s.config.Twitter.Screen_name,
-	}
-	err = s.twitter.Do("GetFriendship", f, &isFriend, 10)
-	if err != nil {
-		s.log.Printf("Can't require identity %d friendship: %s", arg.To_identity.Id, err)
-	}
-
-	if isFriend {
-		dm := &twitter_service.DirectMessagesNewArg{
-			ClientToken:  s.config.Twitter.Client_token,
-			ClientSecret: s.config.Twitter.Client_secret,
-			AccessToken:  s.config.Twitter.Access_token,
-			AccessSecret: s.config.Twitter.Access_secret,
-			Message:      msg,
-			Urls:         urls,
-			ToUserName:   &arg.To_identity.External_username,
-			IdentityId:   &arg.To_identity.Id,
-		}
-		s.twitter.Send("SendDM", dm, 5)
-	} else {
-		tweet := &twitter_service.StatusesUpdateArg{
-			ClientToken:  s.config.Twitter.Client_token,
-			ClientSecret: s.config.Twitter.Client_secret,
-			AccessToken:  s.config.Twitter.Access_token,
-			AccessSecret: s.config.Twitter.Access_secret,
-			Tweet:        fmt.Sprintf("@%s %s", arg.To_identity.External_username, msg),
-			Urls:         urls,
-		}
-		s.twitter.Send("SendTweet", tweet, 5)
 	}
 }
 
