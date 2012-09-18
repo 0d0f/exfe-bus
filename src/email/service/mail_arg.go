73139078a02df830352cdcd9b973a7647beb0f49
Googol Lee <googollee@gmail.com>
2012-09-18 13:57:51 +0800

    quoted code mail attachments filename

--- src/email/service/mail_arg.go
+++ src/email/service/mail_arg.go
@@ -80,7 +80,11 @@ func (m *MailArg) makeMessageWithAttachments() ([]byte, error) {
 	messagePart.Write(message)
 
 	for _, f := range m.FileParts {
-		w1, err := w.CreateFormFile(f.Name, f.Name)
+		name := fmt.Sprintf("=?UTF-8?B?%s?=", base64.StdEncoding.EncodeToString([]byte(f.Name)))
+		header := make(textproto.MIMEHeader)
+		header.Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s\"", name))
+		header.Set("Content-Type", fmt.Sprintf("text/calendar; name=\"%s\"", name))
+		w1, err := w.CreatePart(header)
 		if err != nil {
 			return nil, fmt.Errorf("Create multipart file(%s) fail: %s", f.Name, err)
 		}
