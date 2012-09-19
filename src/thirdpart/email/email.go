efc00f9cfc5ef10295963ae06971516d56352ca3
Googol Lee <googollee@gmail.com>
2012-09-19 15:39:46 +0800

    add email check

--- src/thirdpart/email/email.go
+++ src/thirdpart/email/email.go
@@ -2,21 +2,26 @@ package email
 
 import (
 	"fmt"
-	"net/stmp"
+	"model"
+	"net"
+	"net/smtp"
+	"strings"
 	"thirdpart"
 )
 
 type Email struct {
 	Server string
 	Auth   smtp.Auth
+	From   string
 }
 
 const provider = "email"
 
-func New(host string, port int, username, password string) *Email {
+func New(host string, port int, username, password, from string) *Email {
 	return &Email{
 		Server: fmt.Sprintf("%s:%d", host, port),
 		Auth:   smtp.PlainAuth("", username, password, host),
+		From:   from,
 	}
 }
 
@@ -24,8 +29,8 @@ func (e *Email) Provider() string {
 	return provider
 }
 
-func (e *Email) MessageType() MessageType {
-	return EmailMessage
+func (e *Email) MessageType() thirdpart.MessageType {
+	return thirdpart.EmailMessage
 }
 
 func (e *Email) UpdateFriends(to *model.Identity) error {
@@ -37,5 +42,37 @@ func (e *Email) UpdateIdentity(to *model.Identity) error {
 }
 
 func (e *Email) Send(to *model.Identity, privateMessage string, publicMessage string) error {
-	return smtp.SendMail(e.Server, e.Auth, to.ExternalID, privateMessage)
+	err := e.check(to.ExternalID)
+	if err != nil {
+		return err
+	}
+	return smtp.SendMail(e.Server, e.Auth, e.From, []string{to.ExternalID}, []byte(privateMessage))
+}
+
+func (e *Email) check(mail string) error {
+	mail_split := strings.Split(mail, "@")
+	if len(mail_split) != 2 {
+		return fmt.Errorf("mail(%s) not valid.", mail)
+	}
+	host := mail_split[1]
+	mx, err := net.LookupMX(host)
+	if err != nil {
+		return fmt.Errorf("lookup mail exchange fail: %s", err)
+	}
+	if len(mx) == 0 {
+		return fmt.Errorf("can't find mail exchange for %s", mail)
+	}
+	s, err := smtp.Dial(fmt.Sprintf("%s:25", mx[0].Host))
+	if err != nil {
+		return fmt.Errorf("dial to mail exchange %s fail: %s", mx[0].Host, err)
+	}
+	err = s.Mail(e.From)
+	if err != nil {
+		return fmt.Errorf("set smtp mail fail: %s", err)
+	}
+	err = s.Rcpt(mail)
+	if err != nil {
+		return fmt.Errorf("set smtp rcpt fail: %s", err)
+	}
+	return nil
 }
