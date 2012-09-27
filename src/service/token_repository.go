f6d71099e5fa54d73b0e6d4136bc28177e1e0115
Googol Lee <googollee@gmail.com>
2012-09-27 14:59:59 +0800

    seperate db repo

--- src/service/token_repository.go
+++ src/service/token_repository.go
@@ -6,29 +6,22 @@ import (
 	"database/sql/driver"
 	"fmt"
 	"model"
+	"strings"
 	"time"
 	"tokenmanager"
 )
 
-type TokenRepository struct {
+type DBRepository struct {
 	db     *sql.DB
-	config *model.Config
+	Config *model.Config
 }
 
-func NewTokenRepository(config *model.Config) (*TokenRepository, error) {
-	ret := &TokenRepository{
-		config: config,
-	}
-	err := ret.Connect()
-	return ret, err
-}
-
-func (r *TokenRepository) Connect() error {
+func (r *DBRepository) Connect() error {
 	if r.db != nil {
 		r.db.Close()
 	}
 	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&keepalive=1",
-		r.config.DB.Username, r.config.DB.Password, r.config.DB.Addr, r.config.DB.Port, r.config.DB.DbName))
+		r.Config.DB.Username, r.Config.DB.Password, r.Config.DB.Addr, r.Config.DB.Port, r.Config.DB.DbName))
 	if err != nil {
 		return err
 	}
@@ -41,26 +34,81 @@ func (r *TokenRepository) Connect() error {
 	return nil
 }
 
-// CREATE TABLE `%s` (`id` SERIAL NOT NULL, `key` CHAR(32) NOT NULL, `rand` CHAR(32) NOT NULL, `created_at` DATETIME NOT NULL, `expire_at` DATETIME NOT NULL, `data` TEXT NOT NULL)
+func (r *DBRepository) Query(sql string, v ...interface{}) (*sql.Rows, error) {
+	result, err := r.db.Query(sql, v...)
+	if err == driver.ErrBadConn {
+		err = r.Connect()
+		if err == nil {
+			result, err = r.db.Query(sql, v...)
+		}
+	}
+	return result, err
+}
+
+func (r *DBRepository) Exec(sql string, v ...interface{}) (sql.Result, error) {
+	result, err := r.db.Exec(sql, v...)
+	if err == driver.ErrBadConn {
+		err = r.Connect()
+		if err == nil {
+			result, err = r.db.Exec(sql, v...)
+		}
+	}
+	return result, err
+}
+
+const (
+	CREATE                   = "INSERT INTO `%%table%%` VALUES (null, ?, ?, ?, ?, ?)"
+	STORE                    = "UPDATE `%%table%%` SET expire_at=?, data=? WHERE %%table%%.key=? AND %%table%%.rand=?"
+	FIND_BY_KEY              = "SELECT rand, created_at, expire_at, data FROM `%%table%%` WHERE %%table%%.key=?"
+	FIND_BY_TOKEN            = "SELECT created_at, expire_at, data FROM `%%table%%` WHERE %%table%%.key=? AND %%table%%.rand=?"
+	UPDATE_DATA_BY_TOKEN     = "UPDATE `%%table%%` SET %%table%%.data=? WHERE %%table%%.key=? AND %%table%%.rand=?"
+	UPDATE_EXPIREAT_BY_TOKEN = "UPDATE `%%table%%` SET %%table%%.expire_at=? WHERE %%table%%.key=? AND %%table%%.rand=?"
+	UPDATE_EXPIREAT_BY_KEY   = "UPDATE `%%table%%` SET %%table%%.expire_at=? WHERE %%table%%.key=?"
+	DELETE_BY_TOKEN          = "DELETE FROM `%%table%%` WHERE %%table%%.key=? AND %%table%%.rand=?"
+)
+
+type TokenRepository struct {
+	DBRepository
+	create                string
+	store                 string
+	findByKey             string
+	findByToken           string
+	updateDataByToken     string
+	updateExpireAtByToken string
+	updateExpireAtByKey   string
+	deleteByToken         string
+}
+
+func NewTokenRepository(config *model.Config) (*TokenRepository, error) {
+	ret := &TokenRepository{
+		create:                strings.Replace(CREATE, `%%table%%`, config.TokenManager.TableName, -1),
+		store:                 strings.Replace(STORE, `%%table%%`, config.TokenManager.TableName, -1),
+		findByKey:             strings.Replace(FIND_BY_KEY, `%%table%%`, config.TokenManager.TableName, -1),
+		findByToken:           strings.Replace(FIND_BY_TOKEN, `%%table%%`, config.TokenManager.TableName, -1),
+		updateDataByToken:     strings.Replace(UPDATE_DATA_BY_TOKEN, `%%table%%`, config.TokenManager.TableName, -1),
+		updateExpireAtByToken: strings.Replace(UPDATE_EXPIREAT_BY_TOKEN, `%%table%%`, config.TokenManager.TableName, -1),
+		updateExpireAtByKey:   strings.Replace(UPDATE_EXPIREAT_BY_KEY, `%%table%%`, config.TokenManager.TableName, -1),
+		deleteByToken:         strings.Replace(DELETE_BY_TOKEN, `%%table%%`, config.TokenManager.TableName, -1),
+	}
+	ret.Config = config
+	err := ret.Connect()
+	return ret, err
+}
+
+// CREATE TABLE `%%table%%` (`id` SERIAL NOT NULL, `key` CHAR(32) NOT NULL, `rand` CHAR(32) NOT NULL, `created_at` DATETIME NOT NULL, `expire_at` DATETIME NOT NULL, `data` TEXT NOT NULL)
 
 func (r *TokenRepository) Create(token *tokenmanager.Token) error {
-	const INSERT = "INSERT INTO `%s` VALUES (null, ?, ?, ?, ?, ?)"
-	sql := fmt.Sprintf(INSERT, r.config.TokenManager.TableName)
-	_, err := r.exec(sql, token.Key, token.Rand, r.timeToString(&token.CreatedAt), r.timeToString(token.ExpireAt), token.Data)
+	_, err := r.Exec(r.create, token.Key, token.Rand, r.timeToString(&token.CreatedAt), r.timeToString(token.ExpireAt), token.Data)
 	return err
 }
 
 func (r *TokenRepository) Store(token *tokenmanager.Token) error {
-	const UPDATE = "UPDATE `%s` SET expire_at=?, data=? WHERE %s.key=? AND %s.rand=?"
-	sql := fmt.Sprintf(UPDATE, r.config.TokenManager.TableName, r.config.TokenManager.TableName, r.config.TokenManager.TableName)
-	_, err := r.exec(sql, r.timeToString(token.ExpireAt), token.Data, token.Key, token.Rand)
+	_, err := r.Exec(r.store, r.timeToString(token.ExpireAt), token.Data, token.Key, token.Rand)
 	return err
 }
 
 func (r *TokenRepository) FindByKey(key string) ([]*tokenmanager.Token, error) {
-	const FIND = "SELECT rand, created_at, expire_at, data FROM `%s` WHERE %s.key=?"
-	sql := fmt.Sprintf(FIND, r.config.TokenManager.TableName, r.config.TokenManager.TableName)
-	rows, err := r.db.Query(sql, key)
+	rows, err := r.Query(r.findByKey, key)
 	if err != nil {
 		return nil, err
 	}
@@ -93,9 +141,7 @@ func (r *TokenRepository) FindByKey(key string) ([]*tokenmanager.Token, error) {
 }
 
 func (r *TokenRepository) FindByToken(key, rand string) (*tokenmanager.Token, error) {
-	const FIND = "SELECT created_at, expire_at, data FROM `%s` WHERE %s.key=? AND %s.rand=?"
-	sql := fmt.Sprintf(FIND, r.config.TokenManager.TableName, r.config.TokenManager.TableName, r.config.TokenManager.TableName)
-	rows, err := r.db.Query(sql, key, rand)
+	rows, err := r.Query(r.findByToken, key, rand)
 	if err != nil {
 		return nil, err
 	}
@@ -127,30 +173,22 @@ func (r *TokenRepository) FindByToken(key, rand string) (*tokenmanager.Token, er
 }
 
 func (r *TokenRepository) UpdateDataByToken(key, rand, data string) error {
-	const UPDATE = "UPDATE `%s` SET %s.data=? WHERE %s.key=? AND %s.rand=?"
-	sql := fmt.Sprintf(UPDATE, r.config.TokenManager.TableName, r.config.TokenManager.TableName, r.config.TokenManager.TableName, r.config.TokenManager.TableName)
-	_, err := r.exec(sql, data, key, rand)
+	_, err := r.Exec(r.updateDataByToken, data, key, rand)
 	return err
 }
 
 func (r *TokenRepository) UpdateExpireAtByToken(key, rand string, expireAt *time.Time) error {
-	const UPDATE = "UPDATE `%s` SET %s.expire_at=? WHERE %s.key=? AND %s.rand=?"
-	sql := fmt.Sprintf(UPDATE, r.config.TokenManager.TableName, r.config.TokenManager.TableName, r.config.TokenManager.TableName, r.config.TokenManager.TableName)
-	_, err := r.exec(sql, r.timeToString(expireAt), key, rand)
+	_, err := r.Exec(r.updateExpireAtByToken, r.timeToString(expireAt), key, rand)
 	return err
 }
 
 func (r *TokenRepository) UpdateExpireAtByKey(key string, expireAt *time.Time) error {
-	const UPDATE = "UPDATE `%s` SET %s.expire_at=? WHERE %s.key=?"
-	sql := fmt.Sprintf(UPDATE, r.config.TokenManager.TableName, r.config.TokenManager.TableName, r.config.TokenManager.TableName)
-	_, err := r.exec(sql, r.timeToString(expireAt), key)
+	_, err := r.Exec(r.updateExpireAtByKey, r.timeToString(expireAt), key)
 	return err
 }
 
 func (r *TokenRepository) DeleteByToken(key, rand string) error {
-	const DELETE = "DELETE FROM `%s` WHERE %s.key=? AND %s.rand=?"
-	sql := fmt.Sprintf(DELETE, r.config.TokenManager.TableName, r.config.TokenManager.TableName, r.config.TokenManager.TableName)
-	_, err := r.exec(sql, key, rand)
+	_, err := r.Exec(r.deleteByToken, key, rand)
 	return err
 }
 
@@ -159,15 +197,4 @@ func (r *TokenRepository) timeToString(t *time.Time) string {
 		return "0000-00-00 00:00:00"
 	}
 	return t.UTC().Format(time.RFC3339)
-}
-
-func (r *TokenRepository) exec(sql string, v ...interface{}) (sql.Result, error) {
-	result, err := r.db.Exec(sql, v...)
-	if err == driver.ErrBadConn {
-		err = r.Connect()
-		if err == nil {
-			result, err = r.exec(sql, v...)
-		}
-	}
-	return result, err
 }
