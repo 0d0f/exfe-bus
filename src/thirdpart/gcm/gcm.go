175f297de7750d162f0fd95adb1d4e6b6fe3df5b
Googol Lee <googollee@gmail.com>
2012-10-24 17:49:04 +0800

    update gcm split

--- src/thirdpart/gcm/gcm.go
+++ src/thirdpart/gcm/gcm.go
@@ -5,6 +5,8 @@ import (
 	"formatter"
 	"github.com/googollee/go-gcm"
 	"model"
+	"regexp"
+	"strings"
 	"thirdpart"
 	"unicode/utf8"
 )
@@ -32,40 +34,49 @@ func (g *GCM) MessageType() thirdpart.MessageType {
 }
 
 func (g *GCM) Send(to *model.Recipient, privateMessage string, publicMessage string, data *thirdpart.InfoData) (id string, err error) {
-	cutter, err := formatter.CutterParse(privateMessage, gcmLen)
-	if err != nil {
-		return "", fmt.Errorf("parse cutter error: %s", err)
-	}
-	message := gcm.NewMessage(to.ExternalID)
-	message.SetPayload("badge", "1")
-	message.SetPayload("sound", "")
-	message.SetPayload("cid", fmt.Sprintf("%d", data.CrossID))
-	message.SetPayload("t", data.Type.String())
-	message.DelayWhileIdle = true
-	message.CollapseKey = "exfe"
-
 	ids := ""
-	for _, content := range cutter.Limit(140) {
-		message.SetPayload("text", content)
-		resp, err := g.broker.Send(message)
+	privateMessage = urlRegex.ReplaceAllString(privateMessage, "")
+	for _, line := range strings.Split(privateMessage, "\n") {
+		line = strings.Trim(line, " \r\n\t")
+		cutter, err := formatter.CutterParse(line, gcmLen)
 		if err != nil {
-			return "", fmt.Errorf("send to %s error: %s", to, err)
+			return "", fmt.Errorf("parse cutter error: %s", err)
 		}
+		message := gcm.NewMessage(to.ExternalID)
+		message.SetPayload("badge", "1")
+		message.SetPayload("sound", "")
+		message.SetPayload("cid", fmt.Sprintf("%d", data.CrossID))
+		message.SetPayload("t", data.Type.String())
+		message.DelayWhileIdle = true
+		message.CollapseKey = "exfe"
 
-		fmt.Println(resp.Results)
-		for i := range resp.Results {
-			if resp.Results[i].RegistrationID != to.ExternalID {
-				continue
+		for _, content := range cutter.Limit(140) {
+			message.SetPayload("text", content)
+			resp, err := g.broker.Send(message)
+			if err != nil {
+				return "", fmt.Errorf("send to %s error: %s", to, err)
 			}
-			if err := resp.Results[i].Error; err != "" {
-				return resp.Results[i].MessageID, fmt.Errorf("send to %s error: %s", to, err)
+
+			fmt.Println(resp.Results)
+			for i := range resp.Results {
+				if resp.Results[i].RegistrationID != to.ExternalID {
+					continue
+				}
+				if err := resp.Results[i].Error; err != "" {
+					return resp.Results[i].MessageID, fmt.Errorf("send to %s error: %s", to, err)
+				}
+				ids = fmt.Sprintf("%s,%d", ids, resp.Results[i].MessageID)
 			}
-			ids = fmt.Sprintf("%s,%d", ids, resp.Results[i].MessageID)
 		}
 	}
-	return ids[1:], nil
+	if ids != "" {
+		ids = ids[1:]
+	}
+	return ids, nil
 }
 
 func gcmLen(content string) int {
 	return utf8.RuneCountInString(content)
 }
+
+var urlRegex = regexp.MustCompile(` *(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?`)
--- src/thirdpart/gcm/gcm_test.go
+++ src/thirdpart/gcm/gcm_test.go
@@ -49,14 +49,33 @@ func TestSend(t *testing.T) {
 
 	{
 		broker.Reset()
-		_, err := tester.Send(to, `\(AAAAAAAA name1\), \(AAAAAAAA name2\) and \(AAAAAAAA name3\) are accepted on \(“some cross”\), \(IIIII name1\), \(IIIII name2\) and \(IIIII name3\) interested, \(UUUU name1\), \(UUUU name2\) and \(UUUU name3\) are unavailable, \(PPPPPPP name1\), \(PPPPPPP name2\) and \(PPPPPPP name3\) are pending. \(3 of 10 accepted\). https://exfe.com/#!token=932ce5324321433253`, "public message", data)
+		_, err := tester.Send(to, `\(AAAAAAAA name1\), \(AAAAAAAA name2\) and \(AAAAAAAA name3\) are accepted on \(“some cross”\), \(IIIII name1\), \(IIIII name2\) and \(IIIII name3\) interested, \(UUUU name1\), \(UUUU name2\) and \(UUUU name3\) are unavailable, \(PPPPPPP name1\), \(PPPPPPP name2\) and \(PPPPPPP name3\) are pending. \(3 of 10 accepted\). https://exfe.com/#!token=932ce5324321433253`, "", data)
 		if err != nil {
 			t.Fatalf("send error: %s", err)
 		}
 		results := []string{
-			`AAAAAAAA name1, AAAAAAAA name2 and AAAAAAAA name3 are accepted on “some cross”, IIIII name1, IIIII name2 and IIIII name3 interested,…(1/3)`,
-			`UUUU name1, UUUU name2 and UUUU name3 are unavailable, PPPPPPP name1, PPPPPPP name2 and PPPPPPP name3 are pending. 3 of 10 accepted.…(2/3)`,
-			`https://exfe.com/#!token=932ce5324321433253 (3/3)`,
+			`AAAAAAAA name1, AAAAAAAA name2 and AAAAAAAA name3 are accepted on “some cross”, IIIII name1, IIIII name2 and IIIII name3 interested,…(1/2)`,
+			`UUUU name1, UUUU name2 and UUUU name3 are unavailable, PPPPPPP name1, PPPPPPP name2 and PPPPPPP name3 are pending. 3 of 10 accepted. (2/2)`,
+		}
+		if got, expect := len(broker.messages), len(results); got != expect {
+			t.Errorf("got: %d, expect: %d", got, expect)
+		}
+		for i, r := range results {
+			if got, expect := broker.messages[i], r; got != expect {
+				t.Errorf("%d got: %s, expect %s", i, got, expect)
+			}
+		}
+	}
+
+	{
+		broker.Reset()
+		_, err := tester.Send(to, "post line1\npost line2", "", data)
+		if err != nil {
+			t.Fatalf("send error: %s", err)
+		}
+		results := []string{
+			`post line1`,
+			`post line2`,
 		}
 		if got, expect := len(broker.messages), len(results); got != expect {
 			t.Errorf("got: %d, expect: %d", got, expect)
