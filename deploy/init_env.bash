25187b1692a55f6682c5955b26896f15a0e118f8
Googol Lee <googollee@gmail.com>
2012-11-23 19:28:35 +0800

    update deploy

--- deploy/init_env.bash
+++ deploy/init_env.bash
@@ -6,3 +6,11 @@ mkdir -p /var/run/gobus
 mkdir -p /usr/local/etc/gobus
 
 chown ${U_NAME}:${G_NAME} /var/log/gobus /var/run/gobus
+
+echo RUN="/var/log/gobus" > /etc/init.d/exfe
+echo mkdir -p '$RUN' >> /etc/init.d/exfe
+echo chown ${U_NAME}:${G_NAME} '$RUN' >> /etc/init.d/exfe
+
+chmod +x /etc/init.d/exfe
+
+update-rc.d exfe defaults
