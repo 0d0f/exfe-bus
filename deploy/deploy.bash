362b26dfe8f6a0c71d59bc88d0e1687bc9c28b86
Googol Lee <googollee@gmail.com>
2012-11-23 15:09:52 +0800

    update deploy

--- deploy/deploy.bash
+++ deploy/deploy.bash
@@ -1,7 +1,7 @@
 monit -g gobus stop
 ls /var/run/gobus/exfe_*.pid | while read l; do sudo kill `cat $l`; done
 ls bin/ | while read l; do sudo cp bin/$l /usr/local/bin/exfe_$l; chmod +x /usr/local/bin/exfe_$l; done
-rm -r /usr/local/etc/gobus/template /usr/local/etc/gobus/templates
-cp -r template templates /usr/local/etc/gobus/
+rm -r /usr/local/etc/gobus/templates
+cp -r templates /usr/local/etc/gobus/
 mkdir -p /var/run/gobus
 monit -g gobus start
