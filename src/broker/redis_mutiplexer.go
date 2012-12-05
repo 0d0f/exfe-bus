875cdf9bb3798f836c7577f594522a45600b1380
Googol Lee <googollee@gmail.com>
2012-12-05 17:28:21 +0800

    update redis multiplexer

--- src/broker/redis_mutiplexer.go
+++ src/broker/redis_mutiplexer.go
@@ -1,181 +1,152 @@
 package broker
 
 import (
-	"container/list"
+	"github.com/googollee/go-logger"
+	"github.com/googollee/go-multiplexer"
 	"github.com/googollee/godis"
-	"launchpad.net/tomb"
 	"model"
 	"time"
 )
 
+type RedisInstance struct {
+	redis *godis.Client
+	log   *logger.SubLogger
+}
+
+func (i *RedisInstance) Ping() error {
+	_, err := i.redis.Ping()
+	return err
+}
+
+func (i *RedisInstance) Close() error {
+	return i.redis.Quit()
+}
+
+func (i *RedisInstance) Error(err error) {
+	i.log.Debug("%s", err)
+}
+
 type RedisMultiplexer struct {
-	list    *list.List
-	config  *model.Config
-	get     chan *godis.Client
-	back    chan *godis.Client
-	timeout time.Duration
-	tomb    tomb.Tomb
-	max     int
+	homo   *multiplexer.Homo
+	config *model.Config
 }
 
 func NewRedisMultiplexer(config *model.Config) *RedisMultiplexer {
-	ret := &RedisMultiplexer{
-		list:    list.New(),
-		config:  config,
-		get:     make(chan *godis.Client),
-		back:    make(chan *godis.Client),
-		timeout: time.Duration(config.Redis.HeartBeatInSecond) * time.Second,
-		max:     int(config.Redis.MaxConnections),
-	}
-	if ret.max == 0 {
+	if config.Redis.MaxConnections == 0 {
 		config.Log.Crit("config Redis.MaxConnections should not 0!")
 		panic("config Redis.MaxConnections should not 0!")
 	}
-	for i := 0; i < ret.max; i++ {
-		ret.list.PushBack(ret.createRedis())
+	return &RedisMultiplexer{
+		homo: multiplexer.NewHomo(func() (multiplexer.Instance, error) {
+			return &RedisInstance{
+				redis: godis.New(config.Redis.Netaddr, config.Redis.Db, config.Redis.Password),
+				log:   config.Log.SubPrefix("redis"),
+			}, nil
+		}, config.Redis.MaxConnections, -1, time.Duration(config.Redis.HeartBeatInSecond)*time.Second),
+		config: config,
 	}
-	go looper(ret)
-	return ret
 }
 
 func (m *RedisMultiplexer) Close() error {
-	for i := 0; i < m.max; i++ {
-		redis := <-m.get
-		redis.Quit()
-	}
-
-	m.tomb.Kill(nil)
-	err := m.tomb.Wait()
-	if err != nil {
-		return err
-	}
-	close(m.get)
-	close(m.back)
-	return nil
+	return m.homo.Close()
 }
 
 func (m *RedisMultiplexer) Quit() error {
-	redis := <-m.get
-	defer func() { m.back <- redis }()
 	return nil // no quit
 }
 
-func (m *RedisMultiplexer) Get(key string) (godis.Elem, error) {
-	redis := <-m.get
-	defer func() { m.back <- redis }()
-	return redis.Get(key)
+func (m *RedisMultiplexer) Get(key string) (elem godis.Elem, err error) {
+	m.homo.Do(func(i multiplexer.Instance) {
+		redis := i.(*RedisInstance).redis
+		elem, err = redis.Get(key)
+	})
+	return
 }
 
-func (m *RedisMultiplexer) Set(key string, value interface{}) error {
-	redis := <-m.get
-	defer func() { m.back <- redis }()
-	return redis.Set(key, value)
+func (m *RedisMultiplexer) Set(key string, value interface{}) (err error) {
+	m.homo.Do(func(i multiplexer.Instance) {
+		redis := i.(*RedisInstance).redis
+		err = redis.Set(key, value)
+	})
+	return
 }
 
-func (m *RedisMultiplexer) Del(keys ...string) (int64, error) {
-	redis := <-m.get
-	defer func() { m.back <- redis }()
-	return redis.Del(keys...)
+func (m *RedisMultiplexer) Del(keys ...string) (ret int64, err error) {
+	m.homo.Do(func(i multiplexer.Instance) {
+		redis := i.(*RedisInstance).redis
+		ret, err = redis.Del(keys...)
+	})
+	return
 }
 
-func (m *RedisMultiplexer) Rpush(key string, value interface{}) (int64, error) {
-	redis := <-m.get
-	defer func() { m.back <- redis }()
-	return redis.Rpush(key, value)
+func (m *RedisMultiplexer) Rpush(key string, value interface{}) (ret int64, err error) {
+	m.homo.Do(func(i multiplexer.Instance) {
+		redis := i.(*RedisInstance).redis
+		ret, err = redis.Rpush(key, value)
+	})
+	return
 }
 
-func (m *RedisMultiplexer) Lrange(key string, start, stop int) (*godis.Reply, error) {
-	redis := <-m.get
-	defer func() { m.back <- redis }()
-	return redis.Lrange(key, start, stop)
+func (m *RedisMultiplexer) Lrange(key string, start, stop int) (ret *godis.Reply, err error) {
+	m.homo.Do(func(i multiplexer.Instance) {
+		redis := i.(*RedisInstance).redis
+		ret, err = redis.Lrange(key, start, stop)
+	})
+	return
 }
 
-func (m *RedisMultiplexer) Zadd(key string, score interface{}, member interface{}) (bool, error) {
-	redis := <-m.get
-	defer func() { m.back <- redis }()
-	return redis.Zadd(key, score, member)
+func (m *RedisMultiplexer) Zadd(key string, score interface{}, member interface{}) (ret bool, err error) {
+	m.homo.Do(func(i multiplexer.Instance) {
+		redis := i.(*RedisInstance).redis
+		ret, err = redis.Zadd(key, score, member)
+	})
+	return
 }
 
-func (m *RedisMultiplexer) Zrem(key string, member interface{}) (bool, error) {
-	redis := <-m.get
-	defer func() { m.back <- redis }()
-	return redis.Zrem(key, member)
+func (m *RedisMultiplexer) Zrem(key string, member interface{}) (ret bool, err error) {
+	m.homo.Do(func(i multiplexer.Instance) {
+		redis := i.(*RedisInstance).redis
+		ret, err = redis.Zrem(key, member)
+	})
+	return
 }
 
-func (m *RedisMultiplexer) Zcount(key string, min float64, max float64) (int64, error) {
-	redis := <-m.get
-	defer func() { m.back <- redis }()
-	return redis.Zcount(key, min, max)
+func (m *RedisMultiplexer) Zcount(key string, min float64, max float64) (ret int64, err error) {
+	m.homo.Do(func(i multiplexer.Instance) {
+		redis := i.(*RedisInstance).redis
+		ret, err = redis.Zcount(key, min, max)
+	})
+	return
 }
 
-func (m *RedisMultiplexer) Zscore(key string, member interface{}) (float64, error) {
-	redis := <-m.get
-	defer func() { m.back <- redis }()
-	return redis.Zscore(key, member)
+func (m *RedisMultiplexer) Zscore(key string, member interface{}) (ret float64, err error) {
+	m.homo.Do(func(i multiplexer.Instance) {
+		redis := i.(*RedisInstance).redis
+		ret, err = redis.Zscore(key, member)
+	})
+	return
 }
 
-func (m *RedisMultiplexer) Zrange(key string, start int, stop int) (*godis.Reply, error) {
-	redis := <-m.get
-	defer func() { m.back <- redis }()
-	return redis.Zrange(key, start, stop)
+func (m *RedisMultiplexer) Zrange(key string, start int, stop int) (ret *godis.Reply, err error) {
+	m.homo.Do(func(i multiplexer.Instance) {
+		redis := i.(*RedisInstance).redis
+		ret, err = redis.Zrange(key, start, stop)
+	})
+	return
 }
 
-func (m *RedisMultiplexer) Zrangebyscore(key string, min string, max string, args ...string) (*godis.Reply, error) {
-	redis := <-m.get
-	defer func() { m.back <- redis }()
-	return redis.Zrangebyscore(key, min, max, args...)
+func (m *RedisMultiplexer) Zrangebyscore(key string, min string, max string, args ...string) (ret *godis.Reply, err error) {
+	m.homo.Do(func(i multiplexer.Instance) {
+		redis := i.(*RedisInstance).redis
+		ret, err = redis.Zrangebyscore(key, min, max, args...)
+	})
+	return
 }
 
-func (m *RedisMultiplexer) NewPipeClient() RedisPipe {
-	redis := <-m.get
-	defer func() { m.back <- redis }()
-	return godis.NewPipeClientFromClient(redis)
-}
-
-func (m RedisMultiplexer) createRedis() *godis.Client {
-	return godis.New(m.config.Redis.Netaddr, m.config.Redis.Db, m.config.Redis.Password)
-}
-
-func looper(m *RedisMultiplexer) {
-	defer m.tomb.Done()
-
-	last := time.Now()
-	for {
-		if elem := m.list.Front(); elem != nil {
-			c := elem.Value.(*godis.Client)
-			select {
-			case m.get <- c:
-				m.list.Remove(elem)
-			case r := <-m.back:
-				m.list.PushBack(r)
-			case <-time.After(m.timeout):
-			case <-m.tomb.Dying():
-				return
-			}
-		} else {
-			select {
-			case r := <-m.back:
-				m.list.PushBack(r)
-			case <-time.After(m.timeout):
-			case <-m.tomb.Dying():
-				return
-			}
-		}
-		if time.Now().Sub(last) >= m.timeout {
-			last = time.Now()
-			for i := m.list.Front(); i != nil; i = i.Next() {
-				redis, ok := i.Value.(*godis.Client)
-				if !ok {
-					m.config.Log.Crit("value %s is not godis.Client", i.Value)
-					i.Value = m.createRedis()
-					continue
-				}
-				reply, err := redis.Ping()
-				if err != nil || reply.String() != "PONG" {
-					redis.Quit()
-					i.Value = m.createRedis()
-				}
-			}
-		}
-	}
+func (m *RedisMultiplexer) NewPipeClient() (ret RedisPipe) {
+	m.homo.Do(func(i multiplexer.Instance) {
+		redis := i.(*RedisInstance).redis
+		ret = godis.NewPipeClientFromClient(redis)
+	})
+	return
 }
