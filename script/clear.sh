10d6db3e7e15176d2204be9258e510bfb89f97b0
Googol Lee <googollee@gmail.com>
2012-12-11 15:48:08 +0800

    update script

--- script/clear.sh
+++ script/clear.sh
@@ -8,10 +8,10 @@ DB_NAME=`./config.py '["db"]["username"]'`
 DB_PASS=`./config.py '["db"]["password"]'`
 TK_TABLE=`./config.py '["token_manager"]["table_name"]'`
 
-PASS_ARG="-p\"${DB_PASS}\""
+PASS_ARG="-p${DB_PASS}"
 if [ "${DB_PASS}" = "" ]
 then
 	PASS_ARG=""
 fi
 
-echo "DELETE FROM ${TK_TABLE} WHERE expire_at < DATE_ADD(NOW(), INTERVAL \"-7\" DAY);" | mysql -u"${DB_NAME}" ${PASS_ARG} ${DB}
+echo "DELETE FROM ${TK_TABLE} WHERE expire_at < DATE_ADD(NOW(), INTERVAL \"-7\" DAY);" | mysql -u"${DB_NAME}" "${PASS_ARG}" ${DB}
