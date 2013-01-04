cc68c49b3b70d41e80ebfcbefb2e8999ee567767
Googol Lee <googollee@gmail.com>
2013-01-04 18:37:39 +0800

    new gobus

--- src/gobus/gobus.go
+++ src/gobus/gobus.go
@@ -6,10 +6,204 @@ import (
 	"fmt"
 	"github.com/googollee/go-logger"
 	"github.com/gorilla/mux"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"reflect"
 )
+
+type RouteCreater func() *mux.Route
+
+type Codec interface {
+	Mime() string
+	Decode(r io.Reader, t reflect.Type) (reflect.Value, error)
+	Encode(w io.Writer, v reflect.Value) error
+}
+
+type Service interface {
+	SetRoute(route RouteCreater)
+}
+
+type Server_ struct {
+	router *mux.Router
+	addr   string
+}
+
+func NewServer_(addr string) (*Server_, error) {
+	router := mux.NewRouter()
+	router.StrictSlash(true)
+	return &Server_{
+		router: router,
+		addr:   addr,
+	}, nil
+}
+
+func (s *Server_) Register(service Service) error {
+	service.SetRoute(func() *mux.Route { return s.router.NewRoute() })
+	return nil
+}
+
+func (s *Server_) ListenAndServe() error {
+	h := &http.Server{
+		Addr:    s.addr,
+		Handler: s.router,
+	}
+	return h.ListenAndServe()
+}
+
+func Must(f http.HandlerFunc, err error) http.HandlerFunc {
+	if err != nil {
+		panic(err)
+	}
+	return f
+}
+
+//var typeOfError = reflect.TypeOf((*error)(nil)).Elem()
+var typeOfMap = reflect.TypeOf((map[string]string)(nil))
+
+func callMethod(f reflect.Value, args []reflect.Value) (reflect.Value, error) {
+	rets := f.Call(args)
+	ret, e := rets[0], rets[1].Interface()
+	if e != nil {
+		return ret, e.(error)
+	}
+	return ret, nil
+}
+
+func HandleMethod(codec Codec, arg interface{}, method string) (http.HandlerFunc, error) {
+	t := reflect.TypeOf(arg)
+	v := reflect.ValueOf(arg)
+	m, ok := t.MethodByName(method)
+	if !ok {
+		return nil, fmt.Errorf("can't find method")
+	}
+	if m.Type.NumOut() != 2 {
+		return nil, fmt.Errorf("output arg is not 2")
+	}
+	if m.Type.Out(1) != typeOfError {
+		return nil, fmt.Errorf("second output is not error")
+	}
+
+	switch m.Type.NumIn() {
+	case 3:
+		if m.Type.In(1) != typeOfMap {
+			return nil, fmt.Errorf("first input is not map[string]string")
+		}
+		inputType := m.Type.In(2)
+		inputPtr := false
+		if inputType.Kind() == reflect.Ptr {
+			inputType = inputType.Elem()
+			inputPtr = true
+		}
+		return func(w http.ResponseWriter, r *http.Request) {
+			input, err := codec.Decode(r.Body, inputType)
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write([]byte(err.Error()))
+				return
+			}
+			if !inputPtr {
+				input = input.Elem()
+			}
+			ret, err := callMethod(m.Func, []reflect.Value{v, reflect.ValueOf(Params(r)), input})
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte(err.Error()))
+				return
+			}
+			w.Header().Add("Content-Type", fmt.Sprintf("%s; charset=utf-8", codec.Mime()))
+			err = codec.Encode(w, ret)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte(err.Error()))
+				return
+			}
+			return
+		}, nil
+	case 2:
+		if m.Type.In(1) != typeOfMap {
+			return nil, fmt.Errorf("first input is not map[string]string")
+		}
+		return func(w http.ResponseWriter, r *http.Request) {
+			ret, err := callMethod(m.Func, []reflect.Value{v, reflect.ValueOf(Params(r))})
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte(err.Error()))
+				return
+			}
+			w.Header().Add("Content-Type", fmt.Sprintf("%s; charset=utf-8", codec.Mime()))
+			err = codec.Encode(w, ret)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte(err.Error()))
+				return
+			}
+			return
+		}, nil
+	}
+	return nil, fmt.Errorf("method must have 1 or 2 input arguments")
+}
+
+func Params(r *http.Request) map[string]string {
+	vars := mux.Vars(r)
+	q := r.URL.Query()
+	for k, _ := range q {
+		vars[k] = q.Get(k)
+	}
+	return vars
+}
+
+type Client_ struct {
+	codec      Codec
+	httpClient *http.Client
+}
+
+func NewClient_(codec Codec) *Client_ {
+	return &Client_{
+		codec:      codec,
+		httpClient: http.DefaultClient,
+	}
+}
+
+func (c *Client_) Do(urlStr, method string, arg interface{}, reply interface{}) error {
+	var req *http.Request
+	var err error
+	if arg != nil {
+		reqReader := bytes.NewBuffer(nil)
+		encoder := json.NewEncoder(reqReader)
+		err = encoder.Encode(arg)
+		if err != nil {
+			return err
+		}
+		req, err = http.NewRequest(method, urlStr, reqReader)
+	} else {
+		req, err = http.NewRequest(method, urlStr, nil)
+	}
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", fmt.Sprintf("%s; charset=utf-8", c.codec.Mime()))
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusOK {
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return err
+		}
+		return fmt.Errorf("%s: %s", resp.Status, string(body))
+	}
+	decoder := json.NewDecoder(resp.Body)
+	err = decoder.Decode(reply)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+///////////
 
 type Response interface {
 	Header() http.Header
--- src/gobus/gobus_test.go
+++ src/gobus/gobus_test.go
@@ -3,6 +3,7 @@ package gobus
 import (
 	"fmt"
 	"github.com/googollee/go-logger"
+	"github.com/stretchrcom/testify/assert"
 	"testing"
 )
 
@@ -12,6 +13,20 @@ type gobusTest struct {
 type AddArgs struct {
 	A int
 	B int
+}
+
+func (t *gobusTest) SetRoute(route RouteCreater) {
+	json := new(JSON)
+	route().Methods("POST").Path("/add").HandlerFunc(Must(HandleMethod(json, t, "Add_")))
+	route().Methods("GET").Path("/key/{key}").HandlerFunc(Must(HandleMethod(json, t, "CheckKey_")))
+}
+
+func (t *gobusTest) Add_(params map[string]string, arg AddArgs) (int, error) {
+	return (arg.A + arg.B), nil
+}
+
+func (t *gobusTest) CheckKey_(params map[string]string) (string, error) {
+	return params["key"], nil
 }
 
 func (t *gobusTest) Add(meta *HTTPMeta, args AddArgs, reply *int) error {
@@ -25,6 +40,36 @@ func (t *gobusTest) CheckKey(meta *HTTPMeta, args AddArgs, reply *string) error 
 }
 
 func TestGobus(t *testing.T) {
+	addr := "127.0.0.1:1111"
+
+	s, err := NewServer_(addr)
+	assert.Equal(t, err, nil)
+
+	test := new(gobusTest)
+	err = s.Register(test)
+	assert.Equal(t, err, nil)
+
+	go s.ListenAndServe()
+
+	json := new(JSON)
+	client := NewClient_(json)
+
+	{
+		var reply int
+		err := client.Do(fmt.Sprintf("http://%s/add", addr), "POST", AddArgs{1, 2}, &reply)
+		assert.Equal(t, err, nil)
+		assert.Equal(t, reply, 3)
+	}
+
+	{
+		var reply string
+		err := client.Do(fmt.Sprintf("http://%s/key/abcdefg", addr), "GET", nil, &reply)
+		assert.Equal(t, err, nil)
+		assert.Equal(t, reply, "abcdefg")
+	}
+}
+
+func TestOldGobus(t *testing.T) {
 	const gobusUrl = "http://127.0.0.1:11111"
 	l, err := logger.New(logger.Stderr, "test gobus")
 	if err != nil {
