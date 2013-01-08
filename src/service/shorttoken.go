935e0ffaed697814bbfe500d56be86f5a5c2f6af
Googol Lee <googollee@gmail.com>
2013-01-08 14:36:48 +0800

    update second to seconds

--- src/service/shorttoken.go
+++ src/service/shorttoken.go
@@ -63,15 +63,15 @@ func (s *ShortToken) GET(meta *gobus.HTTPMeta, arg string, reply *[]model.Token)
 }
 
 type UpdateArg struct {
-	Data              *string `json:"data"`
-	ExpireAfterSecond *int    `json:"expire_after_second"`
+	Data               *string `json:"data"`
+	ExpireAfterSeconds *int    `json:"expire_after_seconds"`
 }
 
-// 更新key对应的token的data信息或者expire after second
+// 更新key对应的token的data信息或者expire after seconds
 //
 // 例子：
 //
-//     > curl "http://127.0.0.1:23333/shorttoken/0303?method=PUT" -d '{"data":"xyz","expire_after_second":13}'
+//     > curl "http://127.0.0.1:23333/shorttoken/0303?method=PUT" -d '{"data":"xyz","expire_after_seconds":13}'
 //
 // 返回：
 //
@@ -84,8 +84,8 @@ func (s *ShortToken) PUT(meta *gobus.HTTPMeta, arg UpdateArg, reply *int) error 
 			return err
 		}
 	}
-	if arg.ExpireAfterSecond != nil {
-		after := time.Duration(*arg.ExpireAfterSecond) * time.Second
+	if arg.ExpireAfterSeconds != nil {
+		after := time.Duration(*arg.ExpireAfterSeconds) * time.Second
 		err := s.short.Refresh(key, "", after)
 		if err != nil {
 			return err
@@ -95,20 +95,20 @@ func (s *ShortToken) PUT(meta *gobus.HTTPMeta, arg UpdateArg, reply *int) error 
 }
 
 type ExpireArg struct {
-	Resource          string `json:"resource"`
-	ExpireAfterSecond int    `json:"expire_after_second"`
+	Resource           string `json:"resource"`
+	ExpireAfterSeconds int    `json:"expire_after_seconds"`
 }
 
-// 更新resource对应的token的expire after second
+// 更新resource对应的token的expire after seconds
 //
 // 例子：
 //
-//     > curl "http://127.0.0.1:23333/shorttoken?method=Expire" -d '{"resource":"123","expire_after_second":13}'
+//     > curl "http://127.0.0.1:23333/shorttoken?method=Expire" -d '{"resource":"123","expire_after_seconds":13}'
 //
 // 返回：
 //
 //     0
 func (s *ShortToken) Expire(meta *gobus.HTTPMeta, arg ExpireArg, reply *int) error {
-	after := time.Duration(arg.ExpireAfterSecond) * time.Second
+	after := time.Duration(arg.ExpireAfterSeconds) * time.Second
 	return s.short.Refresh("", arg.Resource, after)
 }
