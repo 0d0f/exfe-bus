a07c1c226b99420c43952062ccae0738e8499aff
Googol Lee <googollee@gmail.com>
2013-02-22 15:29:01 +0800

    update tempalte

--- src/notifier/cross.go
+++ src/notifier/cross.go
@@ -347,7 +347,7 @@ func (a SummaryArg) Link() string {
 }
 
 func (a SummaryArg) PublicLink() string {
-	return fmt.Sprintf("%s/#!%d/%s", a.Config.SiteUrl, a.Cross.ID, a.To.Token[2:6])
+	return fmt.Sprintf("%s/#!%d/%s", a.Config.SiteUrl, a.Cross.ID, a.To.Token[1:5])
 }
 
 func (a *SummaryArg) ListBy(limit int, join string) string {
