8cc51c81b02c9cb017472d7f1658769f861eff69
Googol Lee <googollee@gmail.com>
2013-05-30 14:02:27 +0800

    update example

--- src/service/token.go
+++ src/service/token.go
@@ -46,7 +46,7 @@ type CreateArg struct {
 //
 // 例子：
 //
-//     > curl "http://127.0.0.1:23333/v3/tokens/long" -d '{"data":"abc","resource":"123","expire_after_seconds":300}'
+//     > curl "http://127.0.0.1:23333/v3/tokens?type=long" -d '{"data":"abc","resource":"123","expire_after_seconds":300}'
 //
 // 返回：
 //
