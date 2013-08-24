095782ee2b5bf40328037c3bab2ea41d637136fe
Googol Lee <lzh@exfe.com>
2013-08-25 00:49:54 +0800

    fix distance calculation

--- src/routex/model.go
+++ src/routex/model.go
@@ -15,7 +15,14 @@ import (
 func Distance(latA, lngA, latB, lngB float64) float64 {
 	x := math.Cos(latA*math.Pi/180) * math.Cos(latB*math.Pi/180) * math.Cos((lngA-lngB)*math.Pi/180)
 	y := math.Sin(latA*math.Pi/180) * math.Sin(latB*math.Pi/180)
-	alpha := math.Acos(x + y)
+	s := x + y
+	if s > 1 {
+		s = 1
+	}
+	if s < -1 {
+		s = -1
+	}
+	alpha := math.Acos(s)
 	distance := alpha * 6371000
 	return distance
 }
